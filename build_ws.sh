cec9d53c3a49427c3f79b934ac7f20b5b289d223
mufengjun260 <mufengjun260@gmail.com>
2021-05-17 16:05:10 +0800

    uses the newest version of orocos_kinematics_dynamics to reduce unnecessary function calling

--- build_ws.sh
+++ build_ws.sh
@@ -7,8 +7,7 @@ wstool merge panda_simulator/dependencies.rosinstall
 wstool up
 
 # use old ros-compatible version of kdl
-cd orocos_kinematics_dynamics && git checkout b35c424e77ebc5b7e6f1c5e5c34f8a4666fbf5bc
-cd ../.. && rosdep install -y --from-paths src --ignore-src --rosdistro $ROS_DISTRO
+cd .. && rosdep install -y --from-paths src --ignore-src --rosdistro $ROS_DISTRO
 
 source /opt/ros/$ROS_DISTRO/setup.bash
 catkin build
