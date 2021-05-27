6cec91dcc739df105a845e2f29e361b6c608bb48
JustaGist <saifksidhik@gmail.com>
2021-05-27 13:31:13 +0100

    :bug: [BUGFIX]: Fix gazebo version in dependency; update instructions

--- build_ws.sh
+++ build_ws.sh
@@ -7,8 +7,8 @@ wstool merge panda_simulator/dependencies.rosinstall
 wstool up
 
 # use old ros-compatible version of kdl
-cd orocos_kinematics_dynamics && git checkout b35c424e77ebc5b7e6f1c5e5c34f8a4666fbf5bc
-cd ../.. && rosdep install -y --from-paths src --ignore-src --rosdistro $ROS_DISTRO
+cd orocos_kinematics_dynamics && rm -rf * && git checkout b35c424e && git reset --hard
+cd ../.. && rosdep install -y --from-paths src --ignore-src --rosdistro $ROS_DISTRO --skip-keys python-sip
 
 source /opt/ros/$ROS_DISTRO/setup.bash
 catkin build
